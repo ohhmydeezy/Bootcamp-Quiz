ed7740d9af8f1882ca7a9588853be35dd6315395
OhhMyDeezy <ohhmydeezy@MacDeezy.ultrahub>
2024-01-18 00:54:46 +0000

    begining to add functions to logic.js after completing questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,25 +1,46 @@
 var score = 0;
 var currentQuestion = 0;
+var isWin = false;
 
 
 var questionEl = document.getElementById("questions")
 var questionOptions = document.getElementById("choices")
 var startButton = document.getElementById("startButton")
+var nextButtoon = document.getElementById("next-button")
 
 // Quiz start
 
 function startQuiz() {
-    
-
+    isWin = false;
+    timerCount = 15;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderQuestions()
+    startTimer()
 }
-
-
-//start button event listener function
-
-// timer 
-
 // Multiple choice questions
-
+function renderQuestions() {
+    var currentQuestion = questions[currentQuestion]
+    questionEl.textContent = currentQuestion.questions;
+}
+// timer 
+function startTimer() {
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (isCorrect && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                winGame();
+            }
+        }
+    })
+    };
+    
 // Total score function
 
 // log correct answer/ wrong answer
+
+//start button event listener function
