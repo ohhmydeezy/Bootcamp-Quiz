7edd35f09ec3e7f6eded8497cb4d725e6d5f5062
OhhMyDeezy <ohhmydeezy@MacDeezy.ultrahub>
2024-01-30 18:15:20 +0000

    dynamically added li items to scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,13 +1,30 @@
 var clearButton = document.getElementById("clear")
 
 
+
 function printHighscore() {
-    clearInterval(timer);
-    logHighScore();
+
+    var highscores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+
+    highscores.sort(function(a, b){
+        return 
+    })
+
+    for (var i = 0; i < highscores.length; i++) {
+        var liTag = document.createElement("li");
+        liTag.textContent = `
+            initials: ${highscores[i]["initials"]} 
+            score: ${highscores[i]["score"]}
+            `;
+        var scoreStorage = document.getElementById("highscores");
+        scoreStorage.appendChild(liTag);
+    }
 };
+
 
 function clearHighscores() {
     window.localStorage.removeItem("highScores");
 }
 
 clearButton.onclick = clearHighscores
+printHighscore();
